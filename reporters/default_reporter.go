a288e7e7fe745c39d1e37dfe4e770808ab95e918
letty <letty.ll@alibaba-inc.com>
2023-06-21 12:56:26 +0800

    fix

--- reporters/default_reporter.go
+++ reporters/default_reporter.go
@@ -279,9 +279,7 @@ func (r *DefaultReporter) SuiteDidEnd(report types.Report) {
 					texts = append(texts,fmt.Sprintf("[%s]", specReport.LeafNodeType), specReport.LeafNodeText)
 				}
 				r.emitBlock(r.fi(1, highlightColor+"%s{{/}} %s", heading, strings.Join(texts, " ")))
-			} else {
-				r.emitBlock(r.fi(1, highlightColor+"%s{{/}} %s", heading, fmt.Sprintf("got passed nodetype:%#v", specReport)))
-			}
+			} 
 		}
 	//}
 
